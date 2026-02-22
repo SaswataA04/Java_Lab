915bda09bcb77de8432cdd560ce92fe63710e2d0
Saswata Acharya <infoacaddesk@gmail.com>
2026-02-22 21:59:17 +0530

    updated code

---
+++ n_natural_no_sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumNaturalWhile {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum of first " + n + " natural numbers = " + sum);
+
+        sc.close();
+    }
+}
